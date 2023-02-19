f95513785d1c4f41832895e0df3887c369c1834e
horvi <horvitz@protonmail.ch>
2023-02-19 14:39:02 +0300

    Remake

--- handlers/users/abit_info.py
+++ handlers/users/abit_info.py
@@ -10,17 +10,21 @@ async def start(message: types.Message):
     with open("D:\Desktop\hakaton-itvube\parsing\json\data_file.json", "r",encoding='utf8') as read_file:
         data = json.load(read_file)
 
-
+    answer = []
     title_list = list(data.keys())
     for i in range(len(title_list)):
         compare = func_compare(clean_message(message.text.lower()), title_list[i].lower().split())
         if compare == True:
             text = f'{title_list[i]}\n{data[title_list[i]]}'
             await message.answer(text)
+            answer.append(compare)
+        else:
+            answer.append(compare)
+    await message.answer('Если вы не нашли ответ на ваш вопрос, вы можете обраться за помощью /help')
 
     if ('сайт' or 'ссылка') in clean_message(message.text):
         await message.answer('Ссылки для абитуриентов',reply_markup=kb.site_keyboard)
     elif 'поступить' in clean_message(message.text):
         await message.answer('Задайте более конкретный вопрос')
-    else:
+    elif True not in answer:
         await message.answer('Извините, я не могу ответить на этот вопрос, но вы можете обраться за помощью /help')
